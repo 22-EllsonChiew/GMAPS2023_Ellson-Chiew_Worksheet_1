59a380f4cbff87c201e750cde9666613abd8f324
ItzUzume <ellson637@gmail.com>
2023-12-06 22:08:31 +0800

    Update sonic rotate

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TransformMesh.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TransformMesh.cs
@@ -9,9 +9,9 @@ public class TransformMesh : MonoBehaviour
     public Vector3[] vertices { get; private set; }
 
     private HMatrix2D transformmatrix = new HMatrix2D();
-    HMatrix2D ToOriginMatrix = new HMatrix2D();
-    HMatrix2D FromOriginMatrix = new HMatrix2D();
-    HMatrix2D rotatematrix = new HMatrix2D();
+    HMatrix2D toOriginMatrix = new HMatrix2D();
+    HMatrix2D fromOriginMatrix = new HMatrix2D();
+    HMatrix2D rotateMatrix = new HMatrix2D();
 
     private MeshManager meshManager;
     HVector2D pos = new HVector2D();
@@ -21,7 +21,8 @@ public class TransformMesh : MonoBehaviour
         meshManager = GetComponent<MeshManager>();
         pos = new HVector2D(gameObject.transform.position.x, gameObject.transform.position.y);
 
-        Translate(1, 1);
+       Translate(1, 1);
+       Rotate(45);
     }
 
 
@@ -33,20 +34,42 @@ public class TransformMesh : MonoBehaviour
         Transform();
 
         pos = transformmatrix * pos;
-        
+
 
     }
 
-    /*void Rotate(float angle)
+    void Rotate(float angle)
     {
-        transformmatrix.setIdentity();
+         HMatrix2D toOriginMatrix = new HMatrix2D();
+         HMatrix2D fromOriginMatrix = new HMatrix2D();
+         HMatrix2D rotateMatrix = new HMatrix2D();
 
-        // your code here
+         toOriginMatrix.setTranslationMat(-pos.x, -pos.y);
 
-       // transformmatrix = FromOriginMatrix * // your code here;
+         fromOriginMatrix.setTranslationMat(pos.x, pos.y);
 
-        Transform();
-    }*/
+
+         rotateMatrix.setRotationMat(angle);
+
+         transformmatrix.setIdentity();
+         transformmatrix = fromOriginMatrix * rotateMatrix * toOriginMatrix;
+
+         Transform();
+
+        /*float cosAngle = Mathf.Cos(angle * Mathf.Deg2Rad);
+        float sinAngle = Mathf.Sin(angle * Mathf.Deg2Rad);
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            float x = vertices[i].x - pos.x;
+            float y = vertices[i].y - pos.y;
+
+            vertices[i].x = cosAngle * x - sinAngle * y + pos.x;
+            vertices[i].y = sinAngle * x + cosAngle * y + pos.y;
+        }
+
+        meshManager.clonedMesh.vertices = vertices;*/
+    }
 
     private void Transform()
     {
@@ -55,14 +78,14 @@ public class TransformMesh : MonoBehaviour
         for (int i = 0; i < vertices.Length; i++)
         {
             HVector2D vert = new HVector2D(vertices[i].x, vertices[i].y);
-            Debug.Log(vert.x + " Before ");
+            //Debug.Log(vert.x + " Before ");
             vert = transformmatrix * vert;
 
             
             vertices[i].x = vert.x;
             vertices[i].y = vert.y;
 
-            Debug.Log(vert.x + " After ");
+            //Debug.Log(vert.x + " After ");
         }
 
         meshManager.clonedMesh.vertices = vertices;
